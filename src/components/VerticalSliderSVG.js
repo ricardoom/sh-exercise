e8b6b09eaa7e51d79588fa0b514e23433d03aa1b
Ricardo E. Galvez <galvez667@gmail.com>
2021-08-05 10:14:29 -0700

    :meat_on_bone: Compenent for an individual SVG graphic of the slider

---
+++ src/components/VerticalSliderSVG.js
@@ -0,0 +1,9 @@
+const VerticalSlider = (props) => {
+  return (
+    <svg className={props.className} viewBox='0 0 176 768' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M52 36C52 16.1177 68.1178 0 88 0V0C107.882 0 124 16.1177 124 36V612H52V36Z' />
+      <circle cx='88' cy='680' r='77' />
+    </svg>
+  );
+};
+export default VerticalSlider;
