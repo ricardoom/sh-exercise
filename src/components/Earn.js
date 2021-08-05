6d2e3985259d7cfc80e939f46daf87827db4581b
Ricardo E. Galvez <galvez667@gmail.com>
2021-08-05 10:07:50 -0700

    :meat_on_bone: - better TW integration - update to Earn - renamed form component to form

---
+++ src/components/Earn.js
@@ -0,0 +1,5 @@
+const Earn = () => {
+  return <div>stuff</div>;
+};
+
+export default Earn;
---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import SliderComponent from './SliderComponent';
+import SelectComponent from './SelectComponent';
+
+const FormComponent = (props) => {};
+
+export default FormComponent;
