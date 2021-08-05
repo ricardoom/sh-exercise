f9f930d1b7c9c3c601db8d249c80b2060793a269
Ricardo E. Galvez <galvez667@gmail.com>
2021-08-05 10:43:23 -0700

    :meat_on_bone: building out the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,38 @@
 import { useState } from 'react';
 import Slider from './Slider';
-import Select from './Select';
 
-const FormComponent = (props) => {};
+const FormComponent = (props) => {
+  const [amount, setAmount] = useState('10,000');
+  return (
+    <fieldset>
+      <form>
+        <label htmlFor='dollarAmount'>Enter an amount</label>
+      </form>
+      <div>
+        <input
+          className='rounded-md border-2 border-gray-300 p-2'
+          id='dollarAmount'
+          name='dollarAmount'
+          placeholder='10,000'
+          type='input'
+          onChange={(e) => setAmount(e.target.value)}
+          onBlur={(e) => setAmount(e.target.value)}
+          value={amount}
+        />
+        <span>USD</span>
+      </div>
+      <label htmlFor='currencySelector'>Select Currency</label>
+      <select name='currencySelector' id='currencySelector'>
+        map goes here
+      </select>
+      <span>
+        <label htmlFor='yearly'></label>
+        <select name='yearly' id='yearly'>
+          <option value='1 Year'>1 year</option>
+        </select>
+      </span>
+    </fieldset>
+  );
+};
 
 export default FormComponent;
