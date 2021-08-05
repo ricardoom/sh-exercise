a968fa4c33c89c901972df7ad7825010769768db
Ricardo E. Galvez <galvez667@gmail.com>
2021-08-05 10:28:59 -0700

    basic markup, and stylings

--- src/components/Earn.js
+++ src/components/Earn.js
@@ -1,5 +1,12 @@
+// Earn Component
+import Form from './Form';
 const Earn = () => {
-  return <div>stuff</div>;
+  return (
+    <div className='w-2/5 bg-white rounded-xl shadow-lg flex flex-col p-5 pb-10'>
+      <h3 className='text-2xl text-center my-4'>See how much you can earn</h3>
+      <Form />
+    </div>
+  );
 };
 
 export default Earn;
